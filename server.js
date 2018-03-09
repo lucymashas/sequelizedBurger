0b7034621550b4fd084c022c8aa93c63d5167ab8
Luz Mashas <luzmashas@Luzs-MacBook-Pro.local>
2018-03-08 20:21:53 -0500

    update

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var PORT = process.env.PORT || 3000;
 // Requiring our models for syncing
 var db = require("./models");
 
-// Serve static content for the app from the "public" directory in the application directory.
+// Serve static content for the app from the "public" directory in the application directory...
 app.use(express.static("public"));
 
 // parse application/x-www-form-urlencoded
